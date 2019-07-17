b3f2a20718c3b36ec98d012dc41b6ee398373e8c
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2019-07-17 18:19:25 -0400

    Version préliminaire

--- src/App.js
+++ src/App.js
@@ -7,8 +7,30 @@ export default class App {
 	 * Méthode principale. Sera typiquement appelée après le chargement de la page.
 	 */
 	static main() {
-		console.log("Je suis prêt");
-		document.getElementById("app").innerHTML = "La page est chargée";
+		var app = document.getElementById("app");
+		var timeout = 0;
+		app.addEventListener("mousemove", e => {
+			//console.log(timeout, e.timeStamp);
+			if (e.timeStamp < timeout) {
+				return;
+			}
+			if (e.buttons === 1 || e.shiftKey === true) {
+				var idx = Math.floor(Math.random() * this.persos.length);
+				var image = this.persos[idx];
+				app.appendChild(this.html_Perso(image, e.clientX, e.clientY));
+				timeout = e.timeStamp + Math.random() * 200 + 0;
+			}
+		});
+	}
+	static html_Perso(nom, x, y) {
+		var perso = document.createElement("span");
+		perso.classList.add("perso");
+		perso.style.left = (x + Math.floor(Math.random() * 32) - 16) + "px";
+		perso.style.top = (y + Math.floor(Math.random() * 32) - 16) + "px";
+		perso.style.backgroundImage = "url(images/last-guardian-sprites/" + nom + ".png)";
+		perso.style.backgroundPositionX = Math.floor(Math.random() * 4) * -32 + "px";
+		perso.style.backgroundPositionY = Math.floor(Math.random() * 2) * -32 + "px";
+		return perso;
 	}
 	/**
 	 * Méthode qui permet d'attendre le chargement de la page avant d'éxécuter le script principal
@@ -22,3 +44,15 @@ export default class App {
 		});
 	}
 }
+
+App.persos = [
+	"amg2", "amg3", "amg4", "avt1", "avt2", "avt3", "avt4", "bmg1", "bmg2", "bmg3",
+	"bmg4", "chr1", "dvl1", "ftr1", "ftr2", "ftr3", "ftr4", "gsd1", "isd1", "jli1",
+	"kin1", "knt1", "knt2", "knt3", "knt4", "man1", "man2", "man3", "man4", "mnt1",
+	"mnt2", "mnt3", "mnt4", "mnv1", "mnv2", "mnv3", "mnv4", "mst1", "mst2", "mst3",
+	"mst4", "nja1", "nja2", "nja3", "nja4", "npc1", "npc2", "npc3", "npc4", "npc5",
+	"npc6", "npc7", "npc8", "npc9", "pdn1", "pdn2", "pdn3", "pdn4", "scr1", "scr2",
+	"scr3", "scr4", "skl1", "smr1", "smr2", "smr3", "smr4", "spd1", "syb1", "thf1",
+	"thf2", "thf3", "thf4", "trk1", "wmg1", "wmg2", "wmg3", "wmg4", "wmn1", "wmn2",
+	"wmn3", "wnv1", "wnv2", "wnv3", "wnv4", "ybo1", "ygr1", "zph1", "amg1",
+];
